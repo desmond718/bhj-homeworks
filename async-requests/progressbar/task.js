97ef69d4b1cb21340baf04a6a16d7bb77f6bbedd
evgeny <evgeny.andryukhov@telebreeze.com>
2021-04-30 11:59:09 +0700

    Free task Update

---
+++ async-requests/progressbar/task.js
@@ -0,0 +1,32 @@
+'use strict';
+const progress = document.getElementById( 'progress' );
+const fileUploader = document.getElementById( 'progress' );
+const form = document.getElementById( 'form' );
+form.onsubmit = function upload (event) {
+    event.preventDefault();
+
+    const input  = document.querySelector('input');
+    const file = input.files[0];
+
+    const formData = new FormData(form);
+
+    const xhr = new XMLHttpRequest();
+    xhr.open('POST', 'https://netology-slow-rest.herokuapp.com/upload.php');
+    xhr.setRequestHeader("Content-Type", "multipart/form-data");
+    xhr.setRequestHeader("Cache-Control", "no-cache");
+    // xhr.onprogress = function (event) {
+    //     //progress.value = event.loaded / event.total * 100;
+    //     console.log(event)
+    // }
+    xhr.onload = xhr.onerror = function() {
+        console.log(this.status)
+        if (this.status === 200) {
+            alert('Success');
+        } else {
+            alert('Error');
+        }
+    }
+    console.log('file', file)
+    xhr.send(formData)
+}
+

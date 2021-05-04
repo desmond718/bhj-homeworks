1e707b7f40366e89544041cde61c317fd05d9ab7
lofoo <lofootballer2@mail.ru>
2021-05-04 22:06:41 +0700

    Greate Success Menu additional task

--- element-search/menu/task.js
+++ element-search/menu/task.js
@@ -4,10 +4,15 @@ const arrMenuItem = Array.from(menuItem);
 
 for (let i = 0; i < arrMenuItem.length; i++) {
     arrMenuItem[i].getElementsByClassName('menu__link')[0].onclick = function (event) {
+        const menuActive = document.getElementsByClassName('menu_active');
+        const arrmenuActive = Array.from(menuActive);
         const subMenu = arrMenuItem[i].querySelector('ul');
         if (subMenu) {
             event.preventDefault();
             subMenu.classList.toggle('menu_active');
+        }
+        if (arrmenuActive.length > 0) {
+            arrmenuActive[0].classList.remove('menu_active');
         }
     }
 }

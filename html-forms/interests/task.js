6f65bf63aa2157af391fe8a917793f90d5a2df38
lofoo <lofootballer2@mail.ru>
2021-05-31 23:24:07 +0700

    Update checkbox task

---
+++ html-forms/interests/task.js
@@ -0,0 +1,15 @@
+'use strict';
+const checkbox = Array.from(document.querySelectorAll('.interest'));
+const sisCheckbox = Array.from(document.querySelectorAll('.interests_active .interest__check'));
+console.log(sisCheckbox);
+
+for (let item of checkbox) {
+    item.addEventListener('change', (e) => {
+        console.log(e.target.checked);
+        if (e.target.checked) {
+            for (let item2 of sisCheckbox) {
+                item2.checked = true;
+            }
+        }
+    })
+}

1eb74b3f741192cf405be458aaed13f432ccf151
evgeny <evgeny.andryukhov@telebreeze.com>
2021-06-01 12:33:02 +0700

    Update task interests task

--- html-forms/interests/task.js
+++ html-forms/interests/task.js
@@ -1,19 +1,24 @@
 'use strict';
-const checkbox = Array.from(document.querySelectorAll('.interest'));
-const sisCheckbox = Array.from(document.querySelectorAll('.interests_active .interest__check'));
-console.log(sisCheckbox);
+const checkbox = document.querySelectorAll('.interest__check');
 
-for (let item of checkbox) {
-    item.addEventListener('change', (e) => {
-        console.log(e.target.checked);
-        if (e.target.checked) {
-            for (let item2 of sisCheckbox) {
-                item2.checked = true;
-            }
-        } else {
-            for (let item2 of sisCheckbox) {
-                item2.checked = false;
-            }
-        }
-    })
+function checkAll(dom) {
+  const head = dom.querySelector('label > .interest__check');
+  const status = head.checked;
+  const ul = dom.querySelector('li ul');
+
+  if (ul) {
+    ul.querySelectorAll('.interest__check').forEach((input) => {
+      input.checked = status;
+    });
+  }
 }
+
+checkbox.forEach((input) => {
+  input.addEventListener('change', (e) => {
+    let self = e.target;
+    let li = self.closest('li');
+    if (li) {
+      checkAll(li);
+    }
+  });
+});

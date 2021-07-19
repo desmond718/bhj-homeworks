ab2ec1d0de914d2bc0534f200304322b2b4bc5eb
lofoo <lofootballer2@mail.ru>
2021-07-19 23:14:55 +0700

    Great success Cart task

--- document-structure/cart/task.js
+++ document-structure/cart/task.js
@@ -3,10 +3,22 @@ const product = Array.from(document.getElementsByClassName('product'));
 const cartProducts = document.querySelector('.cart__products');
 
 const myStorage = window.localStorage;
+let objForStorage = {};
+
+if (myStorage.length !== 0) {
+    const cartStorage = JSON.parse(myStorage.getItem('cart'));
+        objForStorage = cartStorage;
+}
 
 product.forEach( (item) => {
     productQuantityControl(item);
     addProductInCart(cartProducts, item);
+
+    for (let key in objForStorage) {
+        if (item.dataset.id == key) {
+            createCartProduct(cartProducts, item, objForStorage);
+        }
+    }
 })
 
 //Управление контролами на карточке продукта
@@ -23,7 +35,6 @@ function productQuantityControl (prod) {
         if (+count.textContent < 1) {
             return null
         }
-
         count.textContent = +count.textContent - 1;
     })
 }
@@ -35,27 +46,36 @@ function addProductInCart (cartProducts, product) {
     const addProductButton = product.querySelector('.product__add');
     addProductButton.addEventListener('click', () => {
 
-        //Проверка на существующий товар в корзине
+        //Проверка на существующий товар в корзине если найден, то меняем значение в корзине
         const cartProduct = Array.from(cartProducts.querySelectorAll('.cart__product'));
         let productIncludes = false;
         cartProduct.forEach( (item) => {
             if (item.dataset.id === product.dataset.id) {
                 let cartCountProduct2 = item.querySelector('.cart__product-count').textContent;
                 let productCount = product.querySelector('.product__quantity-value').textContent;
-                item.querySelector('.cart__product-count').textContent = +cartCountProduct2 + +productCount;
+                let result = item.querySelector('.cart__product-count').textContent = +cartCountProduct2 + +productCount;
                 productIncludes = true;
+
+                //Обнавляем инфу в сторадже
+                objForStorage[item.dataset.id] = result;
+                const obj = JSON.stringify(objForStorage);
+                myStorage.setItem('cart', obj);
             }
         });
 
         //Если товар не найден в корзине, то добавляем его в корзину
         if (!productIncludes) {
-            createNewProduct(cartProducts, product);
+            createCartProduct(cartProducts, product, objForStorage);
         }
     });
 }
 
 //Создает карточку продукта в корзине
-function createNewProduct (cartProducts, product) {
+function createCartProduct (cartProducts, product, storageObj) {
+    //Переменная для првоерки наличия продукта в сторадже
+    let includedStorage = false;
+
+    //Создаем элемент для корзины
     const elementCart = document.createElement('div');
     elementCart.className = 'cart__product';
     elementCart.dataset.id = product.dataset.id;
@@ -66,8 +86,26 @@ function createNewProduct (cartProducts, product) {
 
     const cartCountProduct = document.createElement('div');
     cartCountProduct.className = 'cart__product-count';
-    cartCountProduct.textContent = product.querySelector('.product__quantity-value').textContent;
 
+    //Проверка наличия элемента в сторадже
+    for (let key in storageObj) {
+        if (product.dataset.id == key) {
+            cartCountProduct.textContent = storageObj[elementCart.dataset.id];
+            includedStorage = true;
+        }
+    }
+
+    //Если в сторадже нет элемента то берем значение из DOM
+    if (!includedStorage) {
+        cartCountProduct.textContent = product.querySelector('.product__quantity-value').textContent;
+    }
+
+    //Обнавляем информацию в сторадже
+    objForStorage[elementCart.dataset.id] = +cartCountProduct.textContent;
+    const obj = JSON.stringify(objForStorage);
+    myStorage.setItem('cart', obj);
+
+    //Добавление элемента в DOM
     elementCart.insertAdjacentElement('afterbegin', cartCountProduct);
     elementCart.insertAdjacentElement('afterbegin', imageElementCart);
     cartProducts.insertAdjacentElement('afterbegin', elementCart);
